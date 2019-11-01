68bcd69040988b43aea85faad4d634b42fcefd09
MarioNBushra <mariobushra@outlook.com>
2019-11-01 18:40:03 +0200

    edit

--- src/app.js
+++ src/app.js
@@ -90,7 +90,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Mario Bushra',
         errorMessage: 'Page not found.'
     })
 })
